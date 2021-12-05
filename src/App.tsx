c2fad084b186eb0742690857e5828fc7a33e059c
Josue <jdag98228@gmail.com>
2021-12-05 17:48:08 -0600

    [UPDATE] Custom css of landing page, still work to do, but some correct definitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+function App(){
+    return (
+        <>
+        <div className="header">
+        <img src="Upload.jpg"/>
+        <div className="header-right">
+            <a className="active" href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#contact">Courses</a>
+            <a href="#contact">Unit</a>
+            <a href="#about">Blog</a>
+        </div>
+</div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <h1>Hello World</h1>
+            <Router>
+                <Routes></Routes>
+            </Router>
+        </>
+    )
 }
 
 export default App;
